91471376a542303b6b7a81d24c8f43688ef98524
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2022-10-26 00:31:55 -0400

    Update parsing labels

--- src/parse_label.py
+++ src/parse_label.py
@@ -1,15 +1,72 @@
-import pprint
-from BCBio import GFF
-# from BCBio.GFF import GFFExaminer
+import os 
+import re
+import json 
+def main():
+    mapping_f = open("../Dataset/mapping.json")
+    mapping = json.load(mapping_f)
+    print(mapping)
+    with open("../Dataset/annotations_with_tranx_id.gff", 'r') as f:
+        lists = f.read().splitlines() 
 
-in_file = "../Dataset/GCF_000001405.40_GRCh38.p14_genomic.primary.gff"
-# examiner = GFFExaminer()
-# in_handle = open(in_file)
-# pprint.pprint(examiner.parent_child_map(in_handle))
-# in_handle.close()
+        transcript_id = ""
+        transcript_chr = ""
+        transcript_start = 0
+        transcript_end = 0
+        transcript_strand = "."
+        # prev_transcript_id = ""
 
-print("Hello world!")
-in_handle = open(in_file)
-for rec in GFF.parse(in_handle):
-    print(rec)
-in_handle.close()
+        WRITE = False
+        encoding_ls = [0]
+        for line in lists:
+            line = line.split("\t")
+            # print(len(line))
+            if len(line) < 8:
+                continue
+
+            if (line[2] == "mRNA"):
+                WRITE = True
+                s = ''.join(str(x) for x in encoding_ls)
+                if (transcript_id != ""):
+                    f_w = open("../Dataset/tranx_label/"+transcript_id+".txt", "w")
+                    f_w.write(s)
+                    f_w.close()
+
+                    f_w = open("../Dataset/tranx_label/pos_"+transcript_id+".txt", "w")
+                    f_w.write(transcript_chr + "\t" + str(transcript_start) + "\t" + str(transcript_end))
+                    f_w.close()
+                pattern = ";transcript_id=[\w]*\.[\w]"
+                x = re.search(pattern, line[8])
+                # prev_transcript_id = transcript_id
+                transcript_id = x.group(0)[15:]
+                transcript_chr = line[0]
+                transcript_start = int(line[3])
+                transcript_end = int(line[4])
+                encoding_ls = [mapping["None"]]*(transcript_end-transcript_start+1)
+                transcript_strand = line[6]
+
+            if (line[2] != "mRNA" and line[2] != "exon"):
+                WRITE = False
+
+            if (line[2] == "exon") and WRITE:
+                pattern_e = ";transcript_id=[\w]*\.[\w]"
+                x_e = re.search(pattern_e, line[8])
+                transcript_id_e = x_e.group(0)[15:]
+                if transcript_id == transcript_id_e:
+                    # print(line)
+                    # print("transcript_id_e: ", transcript_id_e)
+                    # print("transcript_id: ", transcript_id)
+                    exon_start = int(line[3])
+                    exon_end = int(line[4])
+
+                    if (transcript_strand == "+"):
+                        encoding_ls[exon_start - transcript_start] = mapping["Acceptor_pos"]
+                        encoding_ls[exon_end - transcript_start] = mapping["Doner_pos"]
+                    elif (transcript_strand == "-"):
+                        encoding_ls[exon_start - transcript_start] = mapping["Doner_neg"]
+                        encoding_ls[exon_end - transcript_start] = mapping["Acceptor_neg"]
+                else:
+                    # Should not enter here!
+                    print(">>>>")
+
+if __name__ == "__main__":
+    main()
