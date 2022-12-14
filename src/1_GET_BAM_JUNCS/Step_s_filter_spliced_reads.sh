441c08a79d3ef860274d5bd80b910b2a0b5733d6
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2022-12-14 16:03:19 -0500

    Update github repo

--- src/1_GET_BAM_JUNCS/Step_s_filter_spliced_reads.sh
+++
@@ -1,3 +0,0 @@
-threshold=100
-seq_len=1000
-awk -v bound="$threshold" '{if($5 > bound && $6 != "?") {print}}' ../junctions_bed/junctions.bed > ../BAM_junctions/${seq_len}bp/junctions_$threshold.bed
--- src_all_isoforms/Step_2_extract_gene_start_end.sh
+++
@@ -1,1 +0,0 @@
-awk '{if($3=="gene"){print}}' ../Dataset/GCF_000001405.40_GRCh38.p14_genomic.gtf | grep "gene_biotype\ \"protein_coding\"" > ../Dataset/all_protein_coding_genes.gtf
