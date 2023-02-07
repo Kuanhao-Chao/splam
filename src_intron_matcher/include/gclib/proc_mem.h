95c739f38483984cce0cad52437664c988f67833
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-02-07 07:26:35 -0500

    Move BAM_clean  to SPLAM

--- src_intron_matcher/include/gclib/proc_mem.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROC_MEM_H
-#define PROC_MEM_H
-#include <stdio.h>
-// a Linux-specific way to report the memory usage of the current process
-void get_mem_usage(double& vm_usage, double& resident_set);
-
-void print_mem_usage(FILE* fout=stderr);
-
-#endif
