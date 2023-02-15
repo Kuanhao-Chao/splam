fd256faeeabde80273f1fab0d1b064c10a63bbaf
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-02-14 23:43:10 -0500

    Update src_SPLAM/SPLAM/ Python scripts

--- src_SPLAM/SPLAM/script/prediction.py
+++ src_SPLAM/SPLAM/script/prediction.py
@@ -1,167 +1,100 @@
-# python prediction.py SRR1352129 ../../../src/MODEL/SpliceAI_6_RB_p_n_nn_n1_TB_all_samples_thr_100_splitByChrom_L64_C16_L800_v31/SpliceNN_19_traced.pt
+#! /usr/bin/python3
+"""All the utility function for SPLAM!
+
+    File name: prediction.py
+    Author: Kuan-Hao Chao
+    Email: kh.chao@cs.jhu.edu
+    Date created: 12/20/2022
+    Date last modified: 01/14/2023
+    Python Version: 3.8
+"""
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-
 import torch
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 from progress.bar import Bar
 import warnings
-
 from splam.dataset import *
 from SpliceNN import *
 from splam.splam_utils import *
 
-############################
-# Log for testing
-############################
-def test_model(epoch_idx, test_loader):
+warnings.filterwarnings("ignore")
+argv = sys.argv[1:]
 
+print("")
+print("################################")
+print("## Start the predictions now! ##")
+print("################################")
+
+#############################
+# Global variable definition
+#############################
+BATCH_SIZE = 100
+N_WORKERS = None
+
+device = torch.device("cuda" if torch.cuda.is_available() else "mps")
+model = torch.load("../../src/MODEL/SpliceAI_6_RB_p_n_nn_n1_TB_all_samples_thr_100_splitByChrom_L64_C16_L800_v31/SpliceNN_19.pt")
+# model = torch.load(argv[1])
+
+#############################
+# Model Initialization
+#############################
+print(f"[Info]: Finish loading model!",flush = True)
+print("SPLAM! model: ", model)
+
+#############################
+# Training Data initialization
+#############################
+test_loader = get_dataloader(BATCH_SIZE, N_WORKERS, "./test_chr9/fasta/junction.fa", True, str(0))
+
+MODEL_OUTPUT_BASE = "../test_chr9/OUTPUT/"
+TARGET_OUTPUT_BASE = MODEL_OUTPUT_BASE + "/"
+LOG_OUTPUT_TEST_BASE = TARGET_OUTPUT_BASE + "LOG/"
+os.makedirs(LOG_OUTPUT_TEST_BASE, exist_ok=True)
+
+
+def test_model(epoch_idx, test_loader):
     criterion = torch.nn.BCELoss()
     ############################
     # Log for testing
     ############################
     junc_scores = TARGET_OUTPUT_BASE + "junc_scores.bed"
-    test_log_loss = LOG_OUTPUT_TEST_BASE + "test_loss.txt"
-
     fw_junc_scores = open(junc_scores, 'w')
-    fw_test_log_loss = open(test_log_loss, 'w')
-
     print("*********************")
     print("** Testing Dataset **")
     print("*********************")
-    epoch_loss = 0
-    epoch_acc = 0
-    # pbar = tqdm(total=len(test_loader))
-    # , ncols=0, desc="Test", unit=" step")
-
-    J_G_TP = 1e-6
-    J_G_FN = 1e-6
-    J_G_FP = 1e-6
-    J_G_TN = 1e-6
-    All_Junction_YL = []
-    All_Junction_YP = []
-
     model.eval()
-    junc_counter = 0
-    
+    junc_counter = 0    
     pbar = Bar('SPLAM! prediction', max=len(test_loader))
-    # for i in range(20):
-    #     # Do some work
-    #     bar.next()
-    # bar.finish()
-
     with torch.no_grad():
         for batch_idx, data in enumerate(test_loader):
             DNAs, labels, seqname = data 
             DNAs = DNAs.to(torch.float32).to(device)
             labels = labels.to(torch.float32).to(device)
-
             DNAs = torch.permute(DNAs, (0, 2, 1))
             loss, accuracy, yps = model_fn(DNAs, labels, model, criterion)
-        
-            #######################################
-            # predicting splice / non-splice
-            #######################################    
-            batch_loss = loss.item()
-            epoch_loss += loss.item()
-            epoch_acc += accuracy
-
             labels = labels.to("cpu").detach().numpy()
             yps = yps.to("cpu").detach().numpy()
-
-            J_G_TP, J_G_FN, J_G_FP, J_G_TN, J_TP, J_FN, J_FP, J_TN = junc_statistics(labels, yps, 0.5, J_G_TP, J_G_FN, J_G_FP, J_G_TN)      
-
-            All_Junction_YL.extend(labels)
-            All_Junction_YP.extend(yps)
-
             pbar.next()
-            # pbar.update(1)
-            # pbar.set_postfix(
-            #     epoch=batch_idx,
-            #     idx_train=len(test_loader)*BATCH_SIZE,
-            #     loss=f"{batch_loss:.6f}",
-            #     accuracy=f"{batch_acc:.6f}",
-            #     J_Precision=f"{J_TP/(J_TP+J_FP+1.e-10):.6f}",
-            #     J_Recall=f"{J_TP/(J_TP+J_FN+1.e-10):.6f}"
-            # )
-
-            fw_test_log_loss.write(str(batch_loss)+ "\n")
-
 
             for idx in range(BATCH_SIZE):
                 junction_score = yps[idx]
-
                 chr, start, end, strand = seqname[idx].split(";")
                 if strand == "+":
                     fw_junc_scores.write(chr+ "\t"+ start + "\t" + end + "\tJUNC_" + str(junc_counter) + "\t0\t"+ strand+ "\t" + str(junction_score) + "\n")
                 elif strand == "-":
                     fw_junc_scores.write(chr+ "\t"+ end + "\t" + start + "\tJUNC_" + str(junc_counter) + "\t0\t"+ strand+ "\t" + str(junction_score) + "\n")
                 junc_counter += 1
-   
+
     pbar.finish()
-    # pbar.close()
-
     fw_junc_scores.close()
-    fw_test_log_loss.close()
-
-
     print(f'Expected #prediction: {len(test_loader)*BATCH_SIZE+0:03}')
-    print("")
-    print("\n\n")
-
-def main():
-    #############################
-    # Model Training
-    #############################
-    # print("Hello world!!!")
-    test_model(0, test_loader)
+    print("\n\n\n")
 
 
 if __name__ == "__main__":
-    warnings.filterwarnings("ignore")
-    argv = sys.argv[1:]
+    test_model(0, test_loader)
 
-    print("")
-    print("################################")
-    print("## Start the predictions now! ##")
-    print("################################")
-
-    #############################
-    # Global variable definition
-    #############################
-    EPOCH_NUM = 20
-    BATCH_SIZE = 100
-    N_WORKERS = None
-    SEQ_LEN="800"
-    QUATER_SEQ_LEN = int(SEQ_LEN)//4
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "mps")
-
-    # SRR1352129_chr9 ../../../src/MODEL/SpliceAI_6_RB_p_n_nn_n1_TB_all_samples_thr_100_splitByChrom_L64_C16_L800_v31/SpliceNN_19.pt
-    # print("argv[1]: ", argv[1])
-
-    model = torch.load("../../src/MODEL/SpliceAI_6_RB_p_n_nn_n1_TB_all_samples_thr_100_splitByChrom_L64_C16_L800_v31/SpliceNN_19.pt")
-    # model = torch.load(argv[1])
-
-    #############################
-    # Model Initialization
-    #############################
-    print(f"[Info]: Finish loading model!",flush = True)
-    print("model: ", model)
-
-    #############################
-    # Training Data initialization
-    #############################
-    TARGET = 'positive'
-    input_fasta = ""
-    test_loader = get_dataloader(BATCH_SIZE, N_WORKERS, "./test_chr9/fasta/junction.fa", True, str(0))
-
-    MODEL_OUTPUT_BASE = "../test_chr9/OUTPUT/"
-    TARGET_OUTPUT_BASE = MODEL_OUTPUT_BASE + "/"
-    LOG_OUTPUT_TEST_BASE = TARGET_OUTPUT_BASE + "LOG/"
-    os.makedirs(LOG_OUTPUT_TEST_BASE, exist_ok=True)
-
-    main()
+# python prediction.py SRR1352129 ../../../src/MODEL/SpliceAI_6_RB_p_n_nn_n1_TB_all_samples_thr_100_splitByChrom_L64_C16_L800_v31/SpliceNN_19_traced.pt
--- src_SPLAM/SPLAM/script/splam/splam_utils.py
+++ src_SPLAM/SPLAM/script/splam/splam_utils.py
@@ -1,7 +1,13 @@
-###############################################################################
-'''This code has functions which process the information in the .h5 files
-datafile_{}_{}.h5 and convert them into a format usable by Keras.'''
-###############################################################################
+#! /usr/bin/python3
+"""All the utility function for SPLAM!
+
+    File name: splam_utils.py
+    Author: Kuan-Hao Chao
+    Email: kh.chao@cs.jhu.edu
+    Date created: 12/20/2022
+    Date last modified: 01/14/2023
+    Python Version: 3.8
+"""
 
 import torch
 from torch.optim.lr_scheduler import LambdaLR
@@ -14,11 +20,8 @@ import random
 import pickle
 from sklearn.metrics import average_precision_score
 
-SEQ_LEN = 800
 JUNC_START = 200
 JUNC_END = 600
-CL_MAX = 10000
-SL = 800
 IN_MAP = np.asarray([[0, 0, 0, 0],
                      [1, 0, 0, 0],
                      [0, 1, 0, 0],
@@ -36,12 +39,9 @@ OUT_MAP = np.asarray([[1, 0, 0],
            
 def one_hot_encode_classifier(Xd):
     return IN_MAP[Xd.astype('int8')]
-           
-#######################################
-# This is for Conformer model 
-#######################################
+
+      
 def create_datapoints(seq, strand):
-    # seq = 'N'*(CL_MAX//2) + seq + 'N'*(CL_MAX//2)
     seq = seq.upper().replace('A', '1').replace('C', '2')
     seq = seq.replace('G', '3').replace('T', '4').replace('N', '0').replace('K', '0').replace('R', '0').replace('Y', '0').replace('M', '0')
     jn_start = JUNC_START
@@ -56,6 +56,7 @@ def create_datapoints(seq, strand):
         Y0 = 1
     X = one_hot_encode_classifier(X0)
     return X, Y0
+
 
 def get_cosine_schedule_with_warmup(
       optimizer: Optimizer,
@@ -85,7 +86,6 @@ def get_cosine_schedule_with_warmup(
     Return:
     :obj:`torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
     """
-
     def lr_lambda(current_step):
         # Warmup
         if current_step < num_warmup_steps:
@@ -99,10 +99,12 @@ def get_cosine_schedule_with_warmup(
         )
     return LambdaLR(optimizer, lr_lambda, last_epoch)
 
+
 def get_accuracy(y_prob, y_true):
     assert y_true.ndim == 1 and y_true.size() == y_prob.size()
     y_prob = y_prob > 0.5
     return (y_true == y_prob).sum().item() / y_true.size(0)
+
 
 def model_fn(DNAs, labels, model, criterion):
     """Forward a batch through the model."""
@@ -110,6 +112,7 @@ def model_fn(DNAs, labels, model, criterion):
     outs = torch.flatten(outs)
     loss, accuracy = categorical_crossentropy_2d(labels, outs, criterion)
     return loss, accuracy, outs
+
 
 def weighted_binary_cross_entropy(output, target, weights=None):    
     if weights is not None:
@@ -120,385 +123,7 @@ def weighted_binary_cross_entropy(output, target, weights=None):
         loss = target * torch.log(output+1e-10) + (1 - target) * torch.log(1 - output+1e-10)
     return torch.neg(torch.mean(loss))
 
+
 def categorical_crossentropy_2d(y_true, y_pred, criterion):
-    # WEIGHT = 800
-    # print("y_true: ", y_true)
-    # print("y_pred: ", y_pred)
     weights = torch.FloatTensor([1.0, 1.0]) 
     return weighted_binary_cross_entropy(y_pred, y_true, weights), get_accuracy(y_pred, y_true)
-    # prod = output[:,0]*target
-    # return -prod[prod<0].sum()
-    # print("y_true: ", y_true)
-    # print("y_pred: ", y_pred)
-    # print("Loss: ", - torch.mean(y_true[:, 0, :]*torch.log(y_pred[:, 0, :]+1e-10)
-    #                     + y_true[:, 1, :]*torch.log(y_pred[:, 1, :]+1e-10)
-    #                     + y_true[:, 2, :]*torch.log(y_pred[:, 2, :]+1e-10)))
-    # print("y_true[:, 0, :]: ", y_true[:, 0, :])
-    # print("y_pred[:, 0, :]: ", y_pred[:, 0, :])
-    # print("y_true[:, 1, :]: ", y_true[:, 1, :])
-    # print("y_pred[:, 1, :]: ", y_pred[:, 1, :])
-    # print("y_true[:, 2, :]: ", y_true[:, 2, :])
-    # print("y_pred[:, 2, :]: ", y_pred[:, 2, :])
-
-    # return - torch.mean(y_true[:, 0, :]*torch.log(y_pred[:, 0, :]+1e-10)
-    #                     + WEIGHT*y_true[:, 1, :]*torch.log(y_pred[:, 1, :]+1e-10)
-    #                     + WEIGHT*y_true[:, 2, :]*torch.log(y_pred[:, 2, :]+1e-10))
-
-def print_top_1_statistics(y_true, y_pred):
-    
-    idx_true = np.nonzero(y_true == 1)[0]
-    argsorted_y_pred = np.argsort(y_pred)
-    sorted_y_pred = np.sort(y_pred)
-
-    top_length = 1
-
-    idx_pred = argsorted_y_pred[-int(top_length*len(idx_true)):]
-    # print(("idx_true: ", idx_true))
-    # print(("idx_pred: ", idx_pred))
-    # print(("np.size(np.intersect1d(idx_true, idx_pred)): ", np.size(np.intersect1d(idx_true, idx_pred))))
-    # print(("float(min(len(idx_pred), len(idx_true))): ", float(min(len(idx_pred), len(idx_true)))))
-    if (len(idx_true) == 0):
-        topkl_accuracy = 0
-    else:
-        topkl_accuracy = np.size(np.intersect1d(idx_true, idx_pred)) \
-                    / float(min(len(idx_pred), len(idx_true)))                
-    auprc = average_precision_score(y_true, y_pred)
-    return topkl_accuracy, auprc
-
-def print_topl_statistics(y_true, y_pred):
-    # Prints the following information: top-kL statistics for k=0.5,1,2,4,
-    # auprc, thresholds for k=0.5,1,2,4, number of true splice sites.
-    # print ("y_true: ", y_true.shape)
-    # print ("y_pred: ", y_pred.shape)
-
-    idx_true = np.nonzero(y_true == 1)[0]
-    # print(("idx_true: ", idx_true))
-    argsorted_y_pred = np.argsort(y_pred)
-    # print(("argsorted_y_pred: ", argsorted_y_pred))
-    # print(("argsorted_y_pred.shape: ", argsorted_y_pred.shape))
-    sorted_y_pred = np.sort(y_pred)
-    # print(("sorted_y_pred: ", sorted_y_pred))
-    # print(("sorted_y_pred.shape: ", sorted_y_pred.shape))
-
-    topkl_accuracy = []
-    threshold = []
-
-    for top_length in [0.5, 1, 2, 4, 10]:
-
-        idx_pred = argsorted_y_pred[-int(top_length*len(idx_true)):]
-        # print(("idx_pred: ", idx_pred))
-        
-        # print(("np.size(np.intersect1d(idx_true, idx_pred)): ", np.size(np.intersect1d(idx_true, idx_pred))))
-        # print(("float(min(len(idx_pred), len(idx_true))): ", float(min(len(idx_pred), len(idx_true)))))
-        topkl_accuracy += [np.size(np.intersect1d(idx_true, idx_pred)) \
-                  / float(min(len(idx_pred), len(idx_true)))]
-        threshold += [sorted_y_pred[-int(top_length*len(idx_true))]] 
-
-    auprc = average_precision_score(y_true, y_pred)
-
-def print_threshold_statistics(y_true, y_pred, threshold, TOTAL_TP, TOTAL_FN, TOTAL_FP, TOTAL_TN):
-    idx_true = np.nonzero(y_true == 1)[0]
-    idx_pred = np.nonzero(y_pred > threshold)[0]
-
-    LCL_TOTAL_TP = np.size(np.intersect1d(idx_true, idx_pred))
-    LCL_TOTAL_FN = len(idx_true) - LCL_TOTAL_TP
-    LCL_TOTAL_FP = len(idx_pred) - LCL_TOTAL_TP
-    LCL_TOTAL_TN = len(y_true) - LCL_TOTAL_TP - LCL_TOTAL_FN - LCL_TOTAL_FP
-
-    # print("LCL_TOTAL_TP: ", LCL_TOTAL_TP)
-    # print("LCL_TOTAL_FN: ", LCL_TOTAL_FN)
-    # print("LCL_TOTAL_FP: ", LCL_TOTAL_FP)
-
-    TOTAL_TP += LCL_TOTAL_TP
-    TOTAL_FN += LCL_TOTAL_FN
-    TOTAL_FP += LCL_TOTAL_FP
-    TOTAL_TN += LCL_TOTAL_TN
-
-    # precision = np.size(np.intersect1d(idx_true, idx_pred)) \
-    #             / float(len(idx_pred))        
-    # recall = np.size(np.intersect1d(idx_true, idx_pred)) \
-    #             / float(len(idx_true)) 
-
-    # print("precision: ", precision)
-    # print("recall:    ", recall)
-    return TOTAL_TP, TOTAL_FN, TOTAL_FP, TOTAL_TN, LCL_TOTAL_TP, LCL_TOTAL_FN, LCL_TOTAL_FP, LCL_TOTAL_TN
-
-def print_junc_statistics(D_YL, A_YL, D_YP, A_YP, threshold, TOTAL_TP, TOTAL_FN, TOTAL_FP, TOTAL_TN):
-
-    label_junc_idx = (D_YL[:, 150]==1) & (A_YL[:, 450]==1)
-    label_nonjunc_idx = (D_YL[:, 150]==0) & (A_YL[:, 450]==0)
-    predict_junc_idx = (D_YP[:, 150]>=threshold) & (A_YP[:, 450]>=threshold)
-    predict_nonjunc_idx = (D_YP[:, 150]<threshold) | (A_YP[:, 450]<threshold)
-
-    idx_true = np.nonzero(label_junc_idx == True)[0]
-    idx_pred = np.nonzero(predict_junc_idx == True)[0]
-
-    # print("idx_true: ", idx_true)
-    # print("idx_pred: ", idx_pred)
-
-    LCL_TOTAL_TP = np.size(np.intersect1d(idx_true, idx_pred))
-    LCL_TOTAL_FN = len(idx_true) - LCL_TOTAL_TP
-    LCL_TOTAL_FP = len(idx_pred) - LCL_TOTAL_TP
-
-    # LCL_TOTAL_TN = np.size(np.intersect1d(label_nonjunc_idx, predict_nonjunc_idx))
-    LCL_TOTAL_TN = len(D_YL) - LCL_TOTAL_TP - LCL_TOTAL_FN - LCL_TOTAL_FP
-
-    # print("LCL_TOTAL_TP: ", LCL_TOTAL_TP)
-    # print("LCL_TOTAL_FN: ", LCL_TOTAL_FN)
-    # print("LCL_TOTAL_FP: ", LCL_TOTAL_FP)
-    # print("LCL_TOTAL_TN: ", LCL_TOTAL_TN)
-
-    TOTAL_TP += LCL_TOTAL_TP
-    TOTAL_FN += LCL_TOTAL_FN
-    TOTAL_FP += LCL_TOTAL_FP
-    TOTAL_TN += LCL_TOTAL_TN
-
-    # precision = np.size(np.intersect1d(idx_true, idx_pred)) \
-    #             / float(len(idx_pred))        
-    # recall = np.size(np.intersect1d(idx_true, idx_pred)) \
-    #             / float(len(idx_true)) 
-
-    # print("precision: ", precision)
-    # print("recall:    ", recall)
-    return TOTAL_TP, TOTAL_FN, TOTAL_FP, TOTAL_TN, LCL_TOTAL_TP, LCL_TOTAL_FN, LCL_TOTAL_FP, LCL_TOTAL_TN
-
-
-
-def junc_statistics(YL, YP, threshold, TOTAL_TP, TOTAL_FN, TOTAL_FP, TOTAL_TN):
-
-    labels_1 = np.where(YL == 1)
-
-    ####################
-    # Donor 
-    ####################
-    thre_d = np.where(YP >= threshold)
-    # print("thre_d: ", thre_d)
-    # print("thre_a: ", thre_a)
-
-    LCL_TOTAL_TP = len(np.intersect1d(labels_1, thre_d))
-    LCL_TOTAL_FN = len(np.setdiff1d(labels_1, thre_d))
-    LCL_TOTAL_FP = len(np.setdiff1d(thre_d, labels_1))
-    LCL_TOTAL_TN = len(YL) - LCL_TOTAL_TP - LCL_TOTAL_FN - LCL_TOTAL_FP
-
-    # print("Donor TPs: ", TPs)
-    # print("Donor FNs: ", FNs)
-    # print("Donor FPs: ", FPs)
-    # print("Donor TNs: ", TNs)
-
-    # LCL_TOTAL_TP = np.size(np.intersect1d(idx_true, idx_pred))
-    # LCL_TOTAL_FN = len(idx_true) - LCL_TOTAL_TP
-    # LCL_TOTAL_FP = len(idx_pred) - LCL_TOTAL_TP
-
-    # # LCL_TOTAL_TN = np.size(np.intersect1d(label_nonjunc_idx, predict_nonjunc_idx))
-    # LCL_TOTAL_TN = len(YL) - LCL_TOTAL_TP - LCL_TOTAL_FN - LCL_TOTAL_FP
-
-    # print("LCL_TOTAL_TP: ", LCL_TOTAL_TP)
-    # print("LCL_TOTAL_FN: ", LCL_TOTAL_FN)
-    # print("LCL_TOTAL_FP: ", LCL_TOTAL_FP)
-    # print("LCL_TOTAL_TN: ", LCL_TOTAL_TN)
-
-    TOTAL_TP += LCL_TOTAL_TP
-    TOTAL_FN += LCL_TOTAL_FN
-    TOTAL_FP += LCL_TOTAL_FP
-    TOTAL_TN += LCL_TOTAL_TN
-
-    # # precision = np.size(np.intersect1d(idx_true, idx_pred)) \
-    # #             / float(len(idx_pred))        
-    # # recall = np.size(np.intersect1d(idx_true, idx_pred)) \
-    # #             / float(len(idx_true)) 
-
-    # # print("precision: ", precision)
-    # # print("recall:    ", recall)
-    return TOTAL_TP, TOTAL_FN, TOTAL_FP, TOTAL_TN, LCL_TOTAL_TP, LCL_TOTAL_FN, LCL_TOTAL_FP, LCL_TOTAL_TN
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Random_90_10 / Chromosome_90_10
-# # TARGET = "Chromosome_split_p_n_nn_n1"
-# SEQ_LEN = "800"
-# # os.makedirs("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/", exist_ok=True)
-
-# def split_seq_name(seq):
-#     return seq[1:]
-
-
-
-# class myDataset(Dataset):
-#     def __init__(self, type, of, shuffle, segment_len=800):
-#         self.segment_len = segment_len
-#         self.data = []
-#         self.indices = []
-#         if type == "train":
-#             CONSTANT_SIZE = 176086
-#         else:
-#             CONSTANT_SIZE = 23914
-
-#         # CONSTANT_SIZE = 500
-#         CONSTANT_SIZE_NEG = math.ceil(CONSTANT_SIZE*2/3)
-#         #################################
-#         ## Processing 'POSITIVE' samples
-#         #################################
-#         pidx = 0
-
-
-#         with open(of, "r") as f:
-#             print("of: ", of)
-#             lines = f.read().splitlines()
-#             seq_name = ""
-#             seq = ""
-#             for line in lines:
-#                 # print(line)
-#                 if pidx % 2 == 0:
-#                     seq_name = split_seq_name(line)
-#                 elif pidx % 2 == 1:
-#                     seq = line
-#                     if seq[0] == ">":
-#                         seq_name = line
-#                         continue
-                    
-#                     X, Y = create_datapoints(seq, '+')
-#                     X = torch.Tensor(np.array(X))
-#                     # print(X)
-#                     # print(Y)
-#                     if X.size()[0] != 800:
-#                         print("seq_name: ", seq_name)
-#                         print(X.size())
-#                         # print(Y.size())
-#                     self.data.append([X, Y, seq_name])
-#                 pidx += 1
-#                 if pidx %10000 == 0:
-#                     print("pidx: ", pidx)
-
-#         index_shuf = list(range(len(self.data)))
-#         if shuffle:
-#             random.shuffle(index_shuf)
-#             # Shuffle just in a certain range.
-
-#         list_shuf = [self.data[i] for i in index_shuf]
-#         self.data = list_shuf 
-#         self.indices = index_shuf
-#         print("pidx: ", pidx)
-
-#     def __len__(self):
-#         return len(self.data)
- 
-#     def __getitem__(self, index):
-#         # Load preprocessed mel-spectrogram.
-#         # print("self.data: ", self.data[index])
-#         feature = self.data[index][0]
-#         label = self.data[index][1]
-#         seq_name = self.data[index][2]
-
-#         feature = torch.flatten(feature, start_dim=1)
-#         return feature, label, seq_name
-
-
-
-# def get_dataloader(batch_size, n_workers, output_file, shuffle):
-#     """Generate dataloader"""
-#     # testset = myDataset("test", output_file, int(SEQ_LEN))
-#     testset = myDataset("test", output_file, shuffle, int(SEQ_LEN))
-
-#     print("testset.indices: ", testset.indices)
-
-#     test_loader = DataLoader(
-#         testset,
-#         batch_size = batch_size,
-#         # batch_size=len(validset),
-#         # num_workers=n_workers,
-#         drop_last=True,
-#         pin_memory=True,
-#         # collate_fn=collate_batch,
-#     )
-#     if batch_size == 1:
-#         print("shuffle: ", shuffle)
-#         torch.save(test_loader, "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.nobatch.pt")
-#         with open("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.nobatch.indices.pkl", "wb") as f:
-#             pickle.dump(testset.indices, f)
-            
-#     elif shuffle:
-#         print("shuffle: ", shuffle)
-#         torch.save(test_loader, "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.shuffle.pt")
-#         with open("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.shuffle.indices.pkl", "wb") as f:
-#             pickle.dump(testset.indices, f)
-
-#     else:
-#         print("shuffle: ", shuffle)
-#         torch.save(test_loader, "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.noshuffle.pt")
-#         with open("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.noshuffle.indices.pkl", "wb") as f:
-#             pickle.dump(testset.indices, f)
-
-#     return test_loader
-
-# # def get_dataloader(batch_size, n_workers):
-# #     # print("Loading dataset: ", "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/"+SEQ_LEN+"bp/"+TARGET+"/train.pt")
-# #     train_loader = torch.load("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/"+SEQ_LEN+"bp/"+TARGET+"/train.pt")
-
-# #     print("Loading dataset: ", "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/"+SEQ_LEN+"bp/"+TARGET+"/test.pt")
-# #     test_loader = torch.load("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/"+SEQ_LEN+"bp/"+TARGET+"/test.pt")
-# #     # return test_loader
-# #     return train_loader, test_loader
-
-# def get_dataloader(batch_size, n_workers, output_file, shuffle, repeat_idx):
-#     """Generate dataloader"""
-#     # testset = myDataset("test", output_file, int(SEQ_LEN))
-#     print("output_file: ", output_file)
-#     testset = myDataset("test", output_file, shuffle, int(SEQ_LEN))
-
-#     # print("testset.indices: ", testset.indices)
-
-#     test_loader = DataLoader(
-#         testset,
-#         batch_size = batch_size,
-#         # batch_size=len(validset),
-#         # num_workers=n_workers,
-#         drop_last=True,
-#         pin_memory=True,
-#         # collate_fn=collate_batch,
-#     )
-#     if batch_size == 1:
-#         print("shuffle: ", shuffle)
-#         # torch.save(test_loader, "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.nobatch."+str(repeat_idx)+"."+str(batch_size)+".pt")
-#         with open("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUT/splam.nobatch.indices."+str(repeat_idx)+"."+str(batch_size)+".pkl", "wb") as f:
-#             pickle.dump(testset.indices, f)
-            
-#     elif shuffle:
-#         print("shuffle: ", shuffle)
-#         # torch.save(test_loader, "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.shuffle."+str(repeat_idx)+"."+str(batch_size)+".pt")
-#         with open("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUT/splam.shuffle.indices."+str(repeat_idx)+"."+str(batch_size)+".pkl", "wb") as f:
-#             pickle.dump(testset.indices, f)
-
-#     else:
-#         print("shuffle: ", shuffle)
-#         # torch.save(test_loader, "/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUTS/SPLAM_v2/test.noshuffle."+str(repeat_idx)+"."+str(batch_size)+".pt")
-#         with open("/Users/chaokuan-hao/Documents/Projects/PR_SPLAM/src_tools_evaluation/INPUT/splam.noshuffle.indices."+str(repeat_idx)+"."+str(batch_size)+".pkl", "wb") as f:
-#             pickle.dump(testset.indices, f)
-
-#     return test_loader
