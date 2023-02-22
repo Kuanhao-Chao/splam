473ab4b1f808ed6cff3df94ec6b2d4a0260106b0
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-02-22 18:37:19 -0500

    Update src/splam_model_scripter.py

---
+++ src/splam_model_scripter.py
@@ -0,0 +1,18 @@
+import torch
+from SPLAM import *
+from splam_utils import *
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as pl
+import os
+
+device = torch.device("cpu")
+model = torch.load("./MODEL/SPLAM_v1/splam_3.pt")
+model.to("cpu")
+print("model: ", model)
+
+# Trace the model with random data.
+example_input = torch.rand(1, 4, 800, device="cpu")
+model_traced = torch.jit.trace(model, example_input)
+
+model_traced.save("./MODEL/SPLAM_v1/splam_3_scripted.pt")
