1f6f4e47b44e207a893ac54140a90aec29ea9606
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-03-19 19:31:07 -0400

    Update extracting alternative splice junctions only

--- src/7_GET_REF_JUNCS_REFSEQ/Step_5_create_x_y.py
+++ src/7_GET_REF_JUNCS_REFSEQ/Step_5_create_x_y.py
@@ -6,7 +6,7 @@ def main():
     QUATER_SEQ_LEN = int(SEQ_LEN) // 4
     THRESHOLD = "100"
     os.makedirs("../INPUTS/"+SEQ_LEN+"bp/", exist_ok=True)
-    fw = open("../INPUTS/"+SEQ_LEN+"bp/input_pos_refseq_protein_isoforms.fa", "w")
+    fw = open("../INPUTS/"+SEQ_LEN+"bp/input_pos_refseq_protein_all.fa", "w")
     fr_donor = open("./REF_junctions/"+SEQ_LEN+"bp/"+THRESHOLD+"_juncs/donor_seq.fa", "r")
     fr_acceptor = open("./REF_junctions/"+SEQ_LEN+"bp/"+THRESHOLD+"_juncs/acceptor_seq.fa", "r")
 
