51aae2fb654bfd2702a547e995a68f91098b3ebf
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-04-29 17:54:33 -0400

    Update src/splam_test_model.py

--- src_tools_evaluation/Step_6_spliceai_prediction_all_seq_N.sh
+++ src_tools_evaluation/Step_6_spliceai_prediction_all_seq_N.sh
@@ -1,7 +1,7 @@
 TARGET=$1
-for (( c=1000; c<=25000; c+=1000 ))
+for (( c=1000; c<=10000; c+=1000 ))
 do
-    echo ">> python Step_7_SpliceAI_prediction_all_seq.py $c N $TARGET"
-    python Step_7_SpliceAI_prediction_all_seq.py $c N $TARGET
+    echo ">> python Step_6_spliceai_prediction_all_seq.py $c N $TARGET"
+    python Step_6_spliceai_prediction_all_seq.py $c N $TARGET
     echo "\n\n"
 done
--- src_tools_evaluation/Step_6_spliceai_prediction_wrapper.sh
+++ src_tools_evaluation/Step_6_spliceai_prediction_wrapper.sh
@@ -1,10 +1,11 @@
-for target in "pos_MANE" "pos_ALTS" "neg_1" "neg_random"
+# for target in "pos_MANE" "pos_ALTS" "neg_1" "neg_random"
+for target in "pos_MANE" "pos_ALTS"
 do
-    echo ./Step_7_SpliceAI_prediction_all_seq_noN.sh $target &
-    ./Step_7_SpliceAI_prediction_all_seq_noN.sh $target &
+    echo ./Step_6_spliceai_prediction_all_seq_noN.sh $target &
+    ./Step_6_spliceai_prediction_all_seq_noN.sh $target &
     echo "\n\n"
 
-    # echo ./Step_7_SpliceAI_prediction_all_seq_N.sh $target &
-    # ./Step_7_SpliceAI_prediction_all_seq_N.sh $target &
-    # echo "\n\n"
+    echo ./Step_6_spliceai_prediction_all_seq_N.sh $target &
+    ./Step_6_spliceai_prediction_all_seq_N.sh $target &
+    echo "\n\n"
 done
