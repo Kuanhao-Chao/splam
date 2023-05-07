773480d77e784c690557e95597f15cff6ce6aaee
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-05-07 10:24:01 -0400

    Update scripts to predict spliceAI (src_tools_evaluation/Step_6_spliceai_prediction_all_seq_N.sh and src_tools_evaluation/Step_6_spliceai_prediction_all_seq_noN.sh)

--- src_tools_evaluation/Step_6_spliceai_prediction_all_seq_noN.sh
+++ src_tools_evaluation/Step_6_spliceai_prediction_all_seq_noN.sh
@@ -1,5 +1,5 @@
 TARGET=$1
-for (( c=1000; c<=10000; c+=1000 ))
+for (( c=1000; c<=25000; c+=1000 ))
 do
     echo ">> python Step_6_spliceai_prediction_all_seq.py $c noN $TARGET"
     python Step_6_spliceai_prediction_all_seq.py $c noN $TARGET
