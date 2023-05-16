32e26858c805488f6258e6bb54dcff98dcc1d3a8
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-05-16 08:46:59 -0400

    update src_splam_cleanup/ scripts

---
+++ src_splam_cleanup/Step_7_sam_index_after.sh
@@ -0,0 +1,13 @@
+# # polyA
+# for sample in R2826 R2835 R2839 R2845 R2855 R2857 R2869 R2874 R2894 R2895 ; do
+#     # Stringtie assembly for after
+#     echo samtools index ../results/polyA/$sample/$sample.cleaned.bam &
+#     samtools index ../results/polyA/$sample/$sample.cleaned.bam &
+# done
+
+# ribozero
+for sample in R12258  R12260  R12263  R12265  R12266  R12277  R12278  R12280  R12285  R12287; do
+    # Stringtie assembly for after
+    echo samtools index ../results/ribozero/$sample/$sample.cleaned.bam &
+    samtools index ../results/ribozero/$sample/$sample.cleaned.bam &
+done
