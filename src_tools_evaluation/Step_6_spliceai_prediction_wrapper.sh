ee7d3073a4a9d6012b387429615cf4ef1c775e7c
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-05-18 22:40:37 -0400

    Update src_tools_evaluation/

--- src_tools_evaluation/Step_6_spliceai_prediction_wrapper.sh
+++ src_tools_evaluation/Step_6_spliceai_prediction_wrapper.sh
@@ -1,5 +1,5 @@
-# for target in "pos_MANE" "pos_ALTS" "neg_1" "neg_random"
-for target in "pos_MANE" "pos_ALTS"
+for target in "pos_MANE" "pos_ALTS" "neg_1" "neg_random"
+#for target in "pos_MANE" "pos_ALTS"
 do
     echo ./Step_6_spliceai_prediction_all_seq_noN.sh $target &
     ./Step_6_spliceai_prediction_all_seq_noN.sh $target &
