d455570770e8fd12226b2f292977e12ba13da85f
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-05-24 13:54:37 -0400

    Update SPLAM_python/RUN_ALL.sh

--- SPLAM_python/RUN_ALL.sh
+++ SPLAM_python/RUN_ALL.sh
@@ -3,18 +3,27 @@ BAM_FILE=./data/SRR1352129_chr9_sub.bam
 MODEL=$3
 
 # Step 1
-echo ./Stepls_1_generate_bed.sh $SAMPLE $BAM_FILE
-./Step_1_generate_bed.sh $SAMPLE $BAM_FILE
+#echo ">> Step 1"
+#echo "../SPLAM_C++/build/splam j-extract -o ./$SAMPLE $BAM_FILE"
+#../SPLAM_C++/build/splam j-extract -o ./$SAMPLE $BAM_FILE
 
-echo python Step_2_get_donors_acceptors_coords.py $SAMPLE
+# Step 2
+echo "\n\n>> Step 2"
+echo "python Step_2_get_donors_acceptors_coords.py $SAMPLE"
 python Step_2_get_donors_acceptors_coords.py $SAMPLE
 
+# Step 3
+echo "\n\n>> Step 3"
 echo ./Step_3_extract_sequences.sh $SAMPLE
 ./Step_3_extract_sequences.sh $SAMPLE
 
+# Step 4
+echo "\n\n>> Step 4"
 echo python Step_4_create_x_y.py $SAMPLE
 python Step_4_create_x_y.py $SAMPLE
 
+# Step 5
+echo "\n\n>> Step 5"
 echo python splam_predict.py -f $SAMPLE/INPUTS/input.fa -o $SAMPLE/junction.score.bed -m ../MODEL/splam_14_script.pt
 python splam_predict.py -f $SAMPLE/INPUTS/input.fa -o $SAMPLE/junction.score.bed -m ../MODEL/splam_14_script.pt
 
