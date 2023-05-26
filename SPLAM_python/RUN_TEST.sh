eaf01d052daf1e2cc7881c9101ae2b6b8f76cb18
Alan Mao <52225670+am12@users.noreply.github.com>
2023-05-26 15:06:56 -0400

    generate scores for 4 databases in test/

---
+++ SPLAM_python/RUN_TEST.sh
@@ -0,0 +1,44 @@
+SAMPLE=tests
+DATABASE=MANE
+
+
+# Step 1
+#echo ">> Step 1"
+#echo "../SPLAM_C++/build/splam j-extract -o ./$SAMPLE $BAM_FILE"
+#../SPLAM_C++/build/splam j-extract -o ./$SAMPLE $BAM_FILE
+
+# Step 2
+echo "\n\n>> Step 2"
+echo "python Step_2_get_donors_acceptors_coords.py $SAMPLE ${DATABASE}_parsed.bed"
+python Step_2_get_donors_acceptors_coords.py $SAMPLE ${DATABASE}_parsed.bed
+
+# Step 3
+echo "\n\n>> Step 3"
+echo ./Step_3_extract_sequences.sh $SAMPLE
+./Step_3_extract_sequences.sh $SAMPLE
+
+# Step 4
+echo "\n\n>> Step 4"
+echo python Step_4_create_x_y.py $SAMPLE
+python Step_4_create_x_y.py $SAMPLE
+
+# Step 5
+echo "\n\n>> Step 5"
+echo python splam_predict.py -f $SAMPLE/INPUTS/input.fa -o $SAMPLE/${DATABASE}.score.bed -m ../MODEL/splam_14_script.pt
+python splam_predict.py -f $SAMPLE/INPUTS/input.fa -o $SAMPLE/${DATABASE}.score.bed -m ../MODEL/splam_14_script.pt
+
+# echo python Step_5_remove_juncs.py $SAMPLE $MODEL
+# python Step_5_remove_juncs.py $SAMPLE $MODEL
+
+# echo ./Step_6_stringtie_assembly.sh $SAMPLE BEFORE $MODEL
+# # ./Step_6_stringtie_assembly.sh $SAMPLE BEFORE $MODEL
+
+# echo ./Step_6_stringtie_assembly.sh $SAMPLE AFTER $MODEL
+# # ./Step_6_stringtie_assembly.sh $SAMPLE AFTER $MODEL
+
+
+# echo ./Step_7_gffcompare.sh $SAMPLE BEFORE  $MODEL ORIGINAL
+# # ./Step_7_gffcompare.sh $SAMPLE BEFORE $MODEL ORIGINAL
+
+# echo ./Step_7_gffcompare.sh $SAMPLE AFTER $MODEL ORIGINAL
+# # ./Step_7_gffcompare.sh $SAMPLE AFTER $MODEL ORIGINAL
