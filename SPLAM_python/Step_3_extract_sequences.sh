fe47789b9965b38b17fd132ed484dccc774c87cb
Alan Mao <52225670+am12@users.noreply.github.com>
2023-06-05 16:01:21 -0400

    full splam on 3 mammalian genomes with figures

--- SPLAM_python/Step_3_extract_sequences.sh
+++ SPLAM_python/Step_3_extract_sequences.sh
@@ -1,12 +1,8 @@
-# if [ $2 = "STAR" ]
-# then
-# bedtools getfasta -s -fi ../Dataset/GRCh38_latest_genomic.fna -bed $1/juncs/donor.bed -fo $1/juncs/donor_seq.fa
 
-# bedtools getfasta -s -fi ../Dataset/GRCh38_latest_genomic.fna -bed $1/juncs/acceptor.bed -fo $1/juncs/acceptor_seq.fa
+# bedtools getfasta -s -fi ../Dataset/GRCh38_p14_genomic_ucsc.fa -bed $1/juncs/donor.bed -fo $1/juncs/donor_seq.fa
 
-# else
-bedtools getfasta -s -fi ../Dataset/GRCh38_p14_genomic_ucsc.fa -bed $1/juncs/donor.bed -fo $1/juncs/donor_seq.fa
+# bedtools getfasta -s -fi ../Dataset/GRCh38_p14_genomic_ucsc.fa -bed $1/juncs/acceptor.bed -fo $1/juncs/acceptor_seq.fa
 
-bedtools getfasta -s -fi ../Dataset/GRCh38_p14_genomic_ucsc.fa -bed $1/juncs/acceptor.bed -fo $1/juncs/acceptor_seq.fa
+bedtools getfasta -s -fi ../Dataset/${2}_genomic.fa -bed $1/juncs/donor.bed -fo $1/juncs/donor_seq.fa
 
-# fi
+bedtools getfasta -s -fi ../Dataset/${2}_genomic.fa -bed $1/juncs/acceptor.bed -fo $1/juncs/acceptor_seq.fa
