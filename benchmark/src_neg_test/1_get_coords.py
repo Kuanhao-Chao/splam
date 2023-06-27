260fa9d6ca839c8dd0b3b5be0426eaef071f2326
am12 <pebblealan@gmail.com>
2023-06-27 15:07:09 -0400

    merge next

--- benchmark/src_neg_test/1_get_coords.py
+++ benchmark/src_neg_test/1_get_coords.py
@@ -1,10 +1,7 @@
 # gets the gene loci start, end, and strand from all the src_neg
 
 import os
-import numpy as np
 import gffutils
-import pandas as pd
-from progress.bar import Bar
 
 def run(input_filename):
     
