f96b0f39469dfc31e5ac293c3b5a1b12ca132e81
am12 <pebblealan@gmail.com>
2023-06-29 17:19:14 -0400

    random sampling and 1-3 pipeline fixed

--- benchmark/src_neg_test/SpliceAI/RUN_ALL.sh
+++ benchmark/src_neg_test/SpliceAI/RUN_ALL.sh
@@ -1,9 +1,10 @@
 DATABASE=TAIR10
+BASE_LEN=800
 
 # Step 1
 echo "\n\n>> Step 1"
-echo python 1_get_spliceai_donors_acceptors_coords.py $DATABASE
-python 1_get_spliceai_donors_acceptors_coords.py $DATABASE
+echo python 1_get_spliceai_donors_acceptors_coords.py $DATABASE $BASE_LEN
+python 1_get_spliceai_donors_acceptors_coords.py $DATABASE $BASE_LEN
 
 # Step 2
 echo "\n\n>> Step 2"
@@ -12,8 +13,8 @@ echo ./2_extract_sequences.sh $DATABASE
 
 # Step 3
 echo "\n\n>> Step 3"
-echo python 3_concatenate_spliceai_input.py $SAMPLE $DATABASE
-python 3_concatenate_spliceai_input.py $SAMPLE $DATABASE
+echo python 3_concatenate_spliceai_input.py $DATABASE
+python 3_concatenate_spliceai_input.py $DATABASE
 
 # Step 4 (technically 6)
-# do this manually...
+# do this manually...cl
