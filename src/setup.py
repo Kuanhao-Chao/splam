1c26b69b5f5e27522da34357f0c2522e445da970
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-07-30 09:45:04 +0200

    Update Splam v1.0.1

--- src/setup.py
+++ src/setup.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 
 # Convert distutils Windows platform specifiers to CMake -A arguments
 PLAT_TO_CMAKE = {
